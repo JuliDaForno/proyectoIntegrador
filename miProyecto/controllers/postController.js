07c6365ec48ca6f1b44e01eef7cd101a218e497e
solapellaniz <sdeapellaniz@udesa.edu.ar>
2022-09-14 13:44:45 -0300

    .

--- miProyecto/controllers/postController.js
+++ miProyecto/controllers/postController.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-const postController = {
-
-    agregar: function(req, res){
-        return res.render('agregarPost')
-    },
-
-    detalle: function(req, res){
-        let idPost = req.params.idPost
-
-        //ahora llamo al db
-        const post = {
-            id_usuario: 2,
-            id: 6,
-            imagen: 'images/adeleposteo2.jpg',
-            nombre: 'Juan Perez'
-        }
-
-        return res.render('detallePost', {post: post})
-    }
-
-}
-
-=======
 const perfil = require('../data/perfil')
 const postController = {
     detallePosteo: function(req,res){
@@ -33,5 +9,4 @@ const postController = {
         res.render('agregarPost')
     }
 }
->>>>>>> e88b060f7d4eeef27100d862f72be6e2c440fa85
 module.exports = postController

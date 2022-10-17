0468ed2dc2f330cd005a2e3a6d441c023cf75a00
isablaquier <iblaquier@udesa.edu.ar>
2022-10-17 12:02:19 -0300

    base de datos

---
+++ miProyecto/models/Comentario.js
@@ -0,0 +1,38 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = 'comentario';
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        id_usuarios: {
+            type: dataTypes.INTEGER
+        },
+        id_posteos: {
+            type: dataTypes.INTEGER
+
+        },
+        texto_comentario: {
+            type: dataTypes.STRING,
+        },
+        created_at: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+    }
+
+    let config ={
+        tableName: "comentario",
+        timestamps: false,
+        underscored: true,
+    }
+
+    let Comentario = sequelize.define(alias,cols, config);
+    
+}
---
+++ miProyecto/models/Usuario.js
@@ -0,0 +1,56 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = 'usuario';
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        nombre: {
+            type: dataTypes.STRING
+        },
+        apellido: {
+            type: dataTypes.STRING
+        },
+        email: {
+            type: dataTypes.STRING
+        },
+        usuario: {
+            type: dataTypes.STRING
+        },
+        contrasenia: {
+            type: dataTypes.STRING
+        },
+        fecha_nacimiento: {
+            type: dataTypes.DATE
+        },
+        numero_documento: {
+            type: dataTypes.INTEGER
+        },
+        foto: {
+            type: dataTypes.STRING
+        },
+        created_at: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        delete_at: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        }
+    }
+
+    let config = {
+        tableName: "usuario",
+        timestamps: false,
+        underscored: true,
+    }
+
+    let Usuario = sequelize.define(alias, cols, config);
+
+}

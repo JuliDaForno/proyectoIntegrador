d44ee5912abbf30d1b1a9f3b550e47bb4ca819b0
JuliDaForno <jdaforno@udesa.edu.ar>
2022-10-18 23:09:25 -0300

    controllers

--- miProyecto/controllers/homeController.js
+++ miProyecto/controllers/homeController.js
@@ -1,11 +1,33 @@
-const data = require('../data')
+const data = require('../models')
+const usuario = data.Usuario;
+const op = data.Sequelize.Op;
+
 const homeController={
     index: function (req,res) {
+        usuario.findAll(criterios)
+        .then((result)=>{
+            return res.render('index', {posteos: result})
+        });
 
-        return res.render('index', {posteos: data.posteos})
+        
     },
-    search: function(req, res){
-        return res.render('resultadoBusqueda')
+    search: (req, res)=>{
+        let id= req.params.id
+        let relaciones = {
+            include:[
+                {
+                    all:true,
+                    nested: true
+                }
+            ]
+        };
+        usuario.findByPk(id, relaciones)
+        .then((result)=>{
+            return res.render('resultadoBusqueda', {usuario:result})
+        })
+        .catch((err)=>{
+            return res.redirect('/')
+        });
     } 
 }
 module.exports = homeController  

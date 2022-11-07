cb4c3aa1d706b7b18b23cf8258a0cfea40e79d1d
JuliDaForno <jdaforno@udesa.edu.ar>
2022-11-07 15:24:09 -0300

    login

--- miProyecto/routes/users.js
+++ miProyecto/routes/users.js
@@ -6,11 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.get('/register',perfilController.register) //create
+router.post('/register', perfilController.store)
 router.get('/perfil/:id', perfilController.detalleUsuario);
 router.get('/miPerfil/:id',perfilController.miPerfil )
 router.get('/login' , perfilController.login)
+router.post('/login', perfilController.loginUsuario)
 
-router.get('/editarPerfil/:id', perfilController.editarPerfil)
+router.post('/editarPerfil/:id', perfilController.editarPerfil)
 
+router.get('/logout', perfilController.logout)
 
 module.exports = router;
